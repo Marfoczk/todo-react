2c9abffb34863231ff0d2fccaf7647c05167c15f
Marcin Głażewski <marfoczok@gmail.com>
2020-05-12 16:35:29 +0200

    Updates

---
+++ precache-manifest.6dc17fa3b05e400ae9530027c0354796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053eab08ab220072dfeddf4c415b8d59",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "871922fcb0f8c4f2a574",
+    "url": "/react-todo-list/static/css/main.fd83de4c.chunk.css"
+  },
+  {
+    "revision": "88ded294b93fe591e7e2",
+    "url": "/react-todo-list/static/js/2.82ba4d70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-list/static/js/2.82ba4d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "871922fcb0f8c4f2a574",
+    "url": "/react-todo-list/static/js/main.754166d4.chunk.js"
+  },
+  {
+    "revision": "3f55eb4d5430f52ab291",
+    "url": "/react-todo-list/static/js/runtime-main.c6e33228.js"
+  }
+]);
